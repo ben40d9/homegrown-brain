a5059cdac1513ee5e737607e5dda205273b8c4fd
ben40d9 <benpaley6@gmail.com>
2023-07-10 00:09:20 -0700

    take old code put at bottom commented out

--- src/utils/processData.js
+++ src/utils/processData.js
@@ -1,25 +1,3 @@
-// export function processData(data) {
-//   return (
-//     data
-
-//       // Remove any rows that don't have a value for the Comment or Reply3 fields
-//       .filter((item) => item.Comment.trim() !== "" && item.Reply3.trim() !== "")
-//       .map((item, index) => {
-//         // console.log(
-//         //   `Processing item ${index}: Comment = ${item.Comment}, Reply3 = ${item.Reply3}`
-//         // );
-//         return {
-//           id: index.toString(),
-//           vector: [
-//             data.map((item) => {
-//               return [item.Comment, item.Reply3];
-//             }),
-//           ],
-//         };
-//       })
-//   );
-// }
-
 // This function categorizes a given comment into one of several predefined categories.
 function categorizeComment(comment) {
   // Convert comment to lower case
@@ -87,3 +65,25 @@ export function processData(data) {
       };
     });
 }
+
+// export function processData(data) {
+//   return (
+//     data
+
+//       // Remove any rows that don't have a value for the Comment or Reply3 fields
+//       .filter((item) => item.Comment.trim() !== "" && item.Reply3.trim() !== "")
+//       .map((item, index) => {
+//         // console.log(
+//         //   `Processing item ${index}: Comment = ${item.Comment}, Reply3 = ${item.Reply3}`
+//         // );
+//         return {
+//           id: index.toString(),
+//           vector: [
+//             data.map((item) => {
+//               return [item.Comment, item.Reply3];
+//             }),
+//           ],
+//         };
+//       })
+//   );
+// }
